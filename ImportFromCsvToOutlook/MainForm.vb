0a1adacb66429338bf5001f85b6cd0f6d3eb75f3
william <william.fromtw@gmail.com>
2020-10-01 03:20:11 +0800

    bug fix , more stable

--- ImportFromCsvToOutlook/MainForm.vb
+++ ImportFromCsvToOutlook/MainForm.vb
@@ -6,42 +6,96 @@ Imports Microsoft.Office.Interop.Outlook
 
 Public Class MainForm
     Dim ffile As OpenFileDialog
+    Dim outlook = GetApplicationObject()
+    Dim ns As Outlook.NameSpace = outlook.GetNamespace("MAPI")
+    Dim aFolder = ns.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderContacts)
+    Dim afolderItems = aFolder.Items
 
-    Private Function CreateDistributionList(strDisplayName As String, strEmail As String)
+    Private Sub CreateDistributionList(strMyToken() As String)
 
-        Dim outlook = GetApplicationObject()
-        Dim ns As Outlook.NameSpace = outlook.GetNamespace("MAPI")
-        Dim aFolder = ns.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderContacts)
-        Dim afolderItems = aFolder.Items
+
         Dim bList As Boolean
         bList = False
-        Dim bMember As Boolean
-        bMember = False
 
         Dim iCount = afolderItems.Count
-        Dim olDistList As Object
+        Dim sTypeName As String
+        Dim sItemObject As Outlook.ContactItem
+
         For x = 1 To iCount
-            If TypeName(afolderItems.Item(x)) = "DistListItem" Then
-                olDistList = afolderItems.Item(x)
+            sTypeName = TypeName(afolderItems.Item(x))
+
+
+
+            If sTypeName = "ContactItem" Then
+                sItemObject = afolderItems.Item(x)
+
+
                 'Check if the distribution list exists
-                If olDistList.DLName = strDisplayName Then
+                If String.IsNullOrEmpty(sItemObject.FirstName) Or String.IsNullOrEmpty(strMyToken(0)) Then
+                    Continue For
+                End If
+                If strMyToken(0).Replace("""", "").Equals(sItemObject.FirstName) Then
                     bList = True
+                    Exit For
                 End If
             End If
         Next x
         If Not bList Then
 
             Dim contact As Outlook.ContactItem = afolderItems.Add(OlItemType.olContactItem)
+            Dim strDisplayName, strFirstName, strLastName, strEmail, strTitle, strDepartment, strOfficeLocation As String
+
+            If strMyToken(0) IsNot Nothing Then
+                strDisplayName = strMyToken(0).Replace("""", "").Trim()
+            Else
+                strDisplayName = ""
+            End If
+            If strMyToken(1) IsNot Nothing And Not strMyToken(1).Equals("") Then
+                strFirstName = strMyToken(1).Replace("""", "").Trim()
+            Else
+                strFirstName = strDisplayName
+            End If
+            If strMyToken(2) IsNot Nothing And Not strMyToken(2).Equals("") Then
+                strLastName = strMyToken(2).Replace("""", "").Trim()
+            Else
+                strLastName = ""
+            End If
+            If strMyToken(4) IsNot Nothing And Not strMyToken(3).Equals("") Then
+                strEmail = strMyToken(3).Replace("""", "").Trim()
+            Else
+                strEmail = ""
+            End If
+            If strMyToken(4) IsNot Nothing And Not strMyToken(4).Equals("") Then
+                strTitle = strMyToken(4).Replace("""", "").Trim()
+            Else
+                strTitle = ""
+            End If
+            If strMyToken(5) IsNot Nothing And Not strMyToken(5).Equals("") Then
+                strDepartment = strMyToken(5).Replace("""", "").Trim()
+            Else
+                strDepartment = ""
+            End If
+            If strMyToken(6) IsNot Nothing And Not strMyToken(6).Equals("") Then
+                strOfficeLocation = strMyToken(6).Replace("""", "").Trim()
+            Else
+                strOfficeLocation = ""
+            End If
+
 
             With contact
-                .FirstName = strDisplayName
+                .FirstName = strFirstName
+                .LastName = strLastName
                 .Email1Address = strEmail
+                .Title = strTitle
+                .Department = strDepartment
+                .OfficeLocation = strOfficeLocation
                 .Save()
             End With
 
             Marshal.ReleaseComObject(contact)
         End If
-    End Function
+
+    End Sub
     Function GetApplicationObject() As Outlook.Application
         Dim application As Outlook.Application
         application = New Outlook.Application()
@@ -49,7 +103,7 @@ Public Class MainForm
     End Function
 
     Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        MsgBox("This APP will import contacts to outlook (2016,2019) , please select csv file ")
         ffile = New OpenFileDialog
         If (ffile.ShowDialog().Equals(DialogResult.OK)) Then
             Dim filePathstring = ffile.FileName
@@ -69,14 +123,15 @@ Public Class MainForm
                 'MsgBox("The first line of the file is " & stringReader)
                 Dim myToken() As String = stringReader.Split(",")
 
-                If myToken.Length <> 2 Then
+                If myToken.Length <> 7 Then
                     MsgBox("line # = " + CStr(iCount) + " in excel is not correct, app will close")
                     Me.Close()
                 Else
-                    CreateDistributionList(myToken(0), myToken(1))
+                    CreateDistributionList(myToken)
                 End If
 
             End While
+            MsgBox("Import contacts finished!")
             Me.Close()
 
         Else
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # ImportFromCsvToOutlook    
-* Import contacts CSV file exported form active directory to outlook (2016 , 2019)     
+* Import contacts to outlook (2016 , 2019)     
 
-### Export vcs From Active Directory
+### Prerequisite    
+* Active Directory 2008R2 or above
+* visual studio 2019 community version
+* outlook 2016 , 2019
+
+### Export vcs From Active Directory by power shell
 Command    
->Get-ADUser -Filter 'mail -like "*<domain>"' -SearchBase "<search base>"  -Properties * | Select -Property   DisplayName,UserPrincipalName | Export-CSV "<csv file location>" -NoTypeInformation -Encoding UTF8    
+>Get-ADUser -Filter 'mail -like "*<domain>"' -SearchBase "<search base>"  -Properties * | Select -Property   DisplayName,GivenName,Surname,mail,Title,Department,Office | Export-CSV "<csv file location>" -NoTypeInformation -Encoding UTF8    
 
 Example    
->Get-ADUser -Filter 'mail -like "*test.com"' -SearchBase "OU=taipei,DC=test,DC=com"  -Properties * | Select -Property   DisplayName,UserPrincipalName | Export-CSV "c:\aaa.csv" -NoTypeInformation -Encoding UTF8    
+>Get-ADUser -Filter 'mail -like "*test.com"' -SearchBase "OU=taipei,DC=test,DC=com"  -Properties * | Select -Property   DisplayName,GivenName,Surname,mail,Title,Department,Office | Export-CSV "c:\aaa.csv" -NoTypeInformation -Encoding UTF8    
